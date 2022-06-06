676e8eae8527223f55096a5144b98b3554772a45
damien berthe <d.berthe1997@laposte.net>
2022-06-06 12:05:20 +0200

    CV Numérique CD UI

---
+++ js/script.js
@@ -0,0 +1,16 @@
+const sr = ScrollReveal();
+
+sr.reveal('h1', {
+    origin: 'top',
+    duration: 2500
+});
+
+
+sr.reveal('i', {
+    duration: 3000,
+});
+
+sr.reveal('img',{
+    origin: 'top',
+    duration: 3000
+});
